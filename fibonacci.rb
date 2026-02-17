c3183a312a6d69e035a3df0cf33e0e2864fcf614
ExMachinery <milkcarrotc@gmail.com>
2026-02-18 04:01:37 +0500

    Both files created and ready to work

---
+++ fibonacci.rb
@@ -0,0 +1,8 @@
+def fibs(n)
+  # Takes n and returns an array n-numbers of Fibonacci sequence
+  # Using traditional iteration method
+end
+
+def fibs_rec(n)
+  # Same, but using recurcive approach
+end
---
+++ merge_sort.rb
@@ -0,0 +1,3 @@
+def merge_sort(array)
+  # Takes an array and returns a sorted array, using recursive approach
+end
